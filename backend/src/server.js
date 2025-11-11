537818e89c492d153021fec87b9b485cdf2af554
Abdelrahman Hossam <49860154+Abdelrahman-Hossam@users.noreply.github.com>
2025-11-11 20:13:34 +0200

    commiting latest changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
+import path from "path";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 // import rateLimiter from "./middleware/rateLimiter.js";
@@ -9,19 +9,31 @@ import { connectDB } from "./config/db.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(express.json()); // middleware gives us access to req.body in notesContoller (It will parse JSON bodies )
-app.use(cors({ origin: "http://localhost:5173" }));
 
-// app.use(rateLimiter); 
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
+// app.use(rateLimiter);
 
 app.use((req, res, next) => {
   // middleware to print the method and URL
   console.log(`req method is ${req.method}, req URL is ${req.url}`);
   next();
 });
+
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist/")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("Server started on PORT:", PORT);
